39f402217cb1117aa88255e4f5296afe059f2ed1
Ueki Masaru <masaru.ueki@itage.co.jp>
2017-02-03 14:38:01 +0900

    init

---
+++ client.cpp
@@ -0,0 +1,14 @@
+#include <waltham-server.h>
+
+#include <cstdint>
+
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+
+int main(int argc, char *argv[])
+{
+    boost::asio::io_service io_service;
+
+    io_service.run();
+    return 0;
+}
